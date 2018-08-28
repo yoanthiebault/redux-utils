af169decb75a0b6f6cb6c50e8625dbd03d1e20c8
Yoan <yoanthiebault@users.noreply.github.com>
2018-08-28 20:10:35 +0200

    Create AsyncValue.ts

---
+++ AsyncValue.ts
@@ -0,0 +1,66 @@
+import {createAction} from "./ReduxType";
+
+export interface AsyncValue<T> {
+    value: T
+    error: any
+    loading: boolean
+}
+
+export module AsyncValueModule {
+    export const INITIAL_ASYNC_VALUE: AsyncValue<any> = {
+        error: null,
+        loading: false,
+        value: null
+    };
+
+
+    export const createLoadActionCreator = <AT extends string>(actionType: AT) => {
+        return () => createAction(actionType)
+    }
+    export const createSuccessActionCreator = <AT extends string,T>(actionType: AT) => {
+        return (value: T) => createAction(actionType, value)
+    }
+    export const createErrorActionCreator = <AT extends string>(actionType: AT) => {
+        return (error: any) => createAction(actionType, error)
+    }
+
+
+    export const createLoadReducer = <S>(attribute: keyof S) => {
+        return (state: S, action) => {
+            const newAsyncValue: AsyncValue<any> = {
+                ...(state as any)[attribute],
+                loading: true
+            }
+            return {
+                ...(state as any),
+                [attribute]: newAsyncValue
+            }
+        }
+    }
+    export const createSuccessReducer = <S extends Object>(attribute: keyof S) => {
+        return (state: S, action: ReturnType<ReturnType<typeof createSuccessActionCreator>>) => {
+            const newAsyncValue: AsyncValue<any> = {
+                value: action.payload,
+                error: null,
+                loading: false
+            }
+            return {
+                ...(state as any),
+                [attribute]: newAsyncValue
+            }
+        }
+    }
+    export const createErrorReducer = <S extends Object>(attribute: keyof S) => {
+        return (state: S, action) => {
+            const newAsyncValue: AsyncValue<any> = {
+                value: null,
+                error: action.payload,
+                loading: false
+            }
+            return {
+                ...(state as any),
+                [attribute]: newAsyncValue
+            }
+        }
+    }
+}
