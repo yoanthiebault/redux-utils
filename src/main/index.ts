064845603898aebb282acddbf301497dfec35172
Yoan Thiebault <yoanthiebault@users.noreply.github.com>
2018-12-07 17:45:13 +0100

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,11 @@
 export * from "./AsyncValueUtils"
 export * from "./ImmutableUtils"
 export * from "./ReduxUtils"
+
+export function classname(...args: string[]): string {
+    return args.filter(arg => arg).join(" ");
+}
+
+export function propertyOf<TObj>(name: keyof TObj): string {
+  return name;
+}
