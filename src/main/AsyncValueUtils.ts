2821ff69c6ca488b06594388eb78c8ba02b95540
ythiebault <yoanthiebault@gmail.com>
2018-12-18 23:10:02 +0100

    Improved typing

--- src/main/AsyncValueUtils.ts
+++ src/main/AsyncValueUtils.ts
@@ -7,6 +7,8 @@ export interface AsyncValue<T> {
     loading: boolean
 }
 
+type Unpack<T> = T extends AsyncValue<infer U> ? U : never
+
 export module AsyncValueUtils {
     import RootReducerManager = ReduxUtils.RootReducerManager;
     const INITIAL_ASYNC_VALUE: AsyncValue<any> = {
@@ -30,10 +32,10 @@ export module AsyncValueUtils {
             }
         }
     };
-    const registerLoadAction = <ST, SL extends keyof ST, SA extends keyof ST[SL], AT extends string>(
+    const registerLoadAction = <ST, SL extends keyof ST, AT extends string>(
         rootReducerManager: RootReducerManager,
         stateLabel: SL & string,
-        stateAttribute: SA,
+        stateAttribute: keyof ST[SL],
         actionType: AT) => {
         const loadActionCreator = createLoadActionCreator(actionType);
         const reducer = createLoadReducer<ST[SL]>(stateAttribute);
@@ -58,14 +60,14 @@ export module AsyncValueUtils {
             }
         }
     };
-    const registerSuccessAction = <ST, SL extends keyof ST & string, SA extends keyof ST[SL], V extends (ST[SL][SA] extends AsyncValue<infer U> ? U : never), AT extends string>(
+    const registerSuccessAction = <ST, SL extends keyof ST & string, SA extends keyof ST[SL], V extends Unpack<ST[SL][SA]>, AT extends string>(
         rootReducerManager: RootReducerManager,
         stateLabel: SL,
         stateAttribute: SA,
         actionType: AT) => {
         const successActionCreator = createSuccessActionCreator<AT, V>(actionType);
         const reducer = createSuccessReducer<ST[SL]>(stateAttribute);
-        rootReducerManager.addReducer<ST,SL,AT,any>(stateLabel, actionType, reducer);
+        rootReducerManager.addReducer<ST, SL, AT, any>(stateLabel, actionType, reducer);
         return successActionCreator
     };
 
@@ -88,39 +90,41 @@ export module AsyncValueUtils {
     };
     const registerErrorAction = <ST, SL extends keyof ST & string, SA extends keyof ST[SL], AT extends string>(
         rootReducerManager: RootReducerManager,
-                                                       stateLabel: SL,
-                                                       stateAttribute: SA,
-                                                       actionType: AT) => {
+        stateLabel: SL,
+        stateAttribute: SA,
+        actionType: AT) => {
         const errorActionCreator = createErrorActionCreator(actionType);
         const reducer = createErrorReducer<ST[SL]>(stateAttribute);
-        rootReducerManager.addReducer<ST,SL,AT,any>(stateLabel, actionType, reducer);
+        rootReducerManager.addReducer<ST, SL, AT, any>(stateLabel, actionType, reducer);
         return errorActionCreator
     };
 
-    export const registerActionCreators = <S, V, LAT extends string, SAT extends string, EAT extends string>(rootReducerManager: RootReducerManager,
-                                                                                                             stateLabel: string,
-                                                                                                             stateAttribute: keyof S,
-                                                                                                             loadActionType: LAT,
-                                                                                                             successActionType: SAT,
-                                                                                                             errorActionType: EAT) => {
+    export const registerActionCreators = <ST, SL extends keyof ST & string, SA extends keyof ST[SL], V extends Unpack<ST[SL][SA]>, LAT extends string, SAT extends string, EAT extends string>(
+        rootReducerManager: RootReducerManager,
+        stateLabel: SL,
+        stateAttribute: SA,
+        loadActionType: LAT,
+        successActionType: SAT,
+        errorActionType: EAT) => {
         // rootReducerManager.setInitialState(stateLabel, INITIAL_ASYNC_VALUE);
         return {
-            loadActionCreator: registerLoadAction(rootReducerManager, stateLabel, stateAttribute, loadActionType),
-            successActionCreator: registerSuccessAction<S, V, SAT>(rootReducerManager, stateLabel, stateAttribute, successActionType),
-            errorActionCreator: registerErrorAction(rootReducerManager, stateLabel, stateAttribute, errorActionType),
+            loadActionCreator: registerLoadAction<ST, SL, LAT>(rootReducerManager, stateLabel, stateAttribute, loadActionType),
+            successActionCreator: registerSuccessAction<ST, SL, SA, V, SAT>(rootReducerManager, stateLabel, stateAttribute, successActionType),
+            errorActionCreator: registerErrorAction<ST, SL, SA, EAT>(rootReducerManager, stateLabel, stateAttribute, errorActionType),
         }
     };
 
-    export const registerThunkActionCreator = <S, V, U extends any[]>(rootReducerManager: RootReducerManager,
-                                                                      stateLabel: string,
-                                                                      stateAttribute: keyof S,
-                                                                      actionPrefix: string,
-                                                                      asyncFunction: (rootState: S, ...args: U) => Promise<V>): (...args: U) => ThunkAction<void, S, void, Action<any>> => {
+    export const registerThunkActionCreator = <ST, SL extends keyof ST & string, SA extends keyof ST[SL], V extends Unpack<ST[SL][SA]>, U extends any[]>(
+        rootReducerManager: RootReducerManager,
+        stateLabel: SL,
+        stateAttribute: SA,
+        actionPrefix: string,
+        asyncFunction: (rootState: ST, ...args: U) => Promise<V>): (...args: U) => ThunkAction<void, ST, void, Action<any>> => {
         const {
             loadActionCreator,
             successActionCreator,
             errorActionCreator
-        } = registerActionCreators(rootReducerManager, stateLabel, stateAttribute,
+        } = registerActionCreators<ST,SL,SA,V,string,string,string>(rootReducerManager, stateLabel, stateAttribute,
             actionPrefix + "/load",
             actionPrefix + "/success",
             actionPrefix + "/error");
